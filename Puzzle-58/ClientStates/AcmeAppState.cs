89e156113d7255a2c98339a51353e8aa7ca5ea3d
John Maguire <31592841+johnpmaguire@users.noreply.github.com>
2024-12-09 19:49:58 +0000

    2nd attemp

---
+++ Puzzle-58/ClientStates/AcmeAppState.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace Puzzle_58.Components
+{
+    public partial class AcmeAppState : ComponentBase
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        public string AcmeAppStateText = "This is the home page for ACME.";
+        public string AcmeAppConfigPageStateText = "This is the Config page for ACME.";
+
+       
+    }
+}
---
+++ Puzzle-58/ClientStates/ContosoAppState.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Puzzle_58.Components
+{
+    public partial class ContosoAppState : ComponentBase
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        public string ContosoAppStateText = "This is the home page for Contoso.";
+        public string ContosoAppConfigPageStateText = "This is the Config page for Contoso.";
+
+        // Add properties here that you want to share across Contoso client pages
+    }
+}
